ea60f2c146f52bc1ded754e5fd3880631899ed1d
Mogileus <145678055+Mogileus@users.noreply.github.com>
2023-11-06 11:50:56 +0200

    Test5.java

--- src/main/java/org/chdtu/Test5.java
+++ src/main/java/org/chdtu/Test5.java
@@ -6,8 +6,8 @@ public class Test5 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
-        Cat myCat = context.getBean("myPet", Cat.class);
-        myCat.say();
+        Magazine myMagazine = context.getBean("myLibrary", Magazine.class);
+        myMagazine.say();
 
         context.close();
     }
